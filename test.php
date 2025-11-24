0c3e5b7765fc67277d265f7e5172ea23b53a0c60
Ketari <admin@interlink-impact.com>
2025-11-24 14:03:15 +0800

    Update test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$conn = new mysqli("localhost", "pelangai_pelangai", "Amizar77@1973", "pelangai_pelangai");
-
-    if($conn->connect_error){
-        die("Connection failed: " . $conn->connect_error);
-    }
-?>
