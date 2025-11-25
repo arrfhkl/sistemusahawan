0194fe47ca7eea7d33de2d629ffcc6e1fb13b77f
Ketari <admin@interlink-impact.com>
2025-11-25 10:29:06 +0800

    Update admin_agro.php

--- admin_agro.php
+++ admin_agro.php
@@ -35,6 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["id"], $_POST["status"
     exit;
 }
 
+
 // ====== Pagination Setup ======
 $limit = 15; // jumlah data per halaman
 $page = isset($_GET["page"]) ? (int)$_GET["page"] : 1;
